dc6accc7da0e1cede4714cd9ac6f4aa8a16f0119
enzo253 <139024939+enzo253@users.noreply.github.com>
2025-03-22 13:20:12 +0100

    just started storing logic for redis

---
+++ DataStore.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_KEYS 100
+
+typedef struct {
+	char key [50];
+	char value[100];
+	time_t expiry;
+} KeyValue;
+
+keyValue store[MAX_KEYS];
+
+void set(const char *key, const char *value, int expiry) {
+	for (int i = 0; i < MAX_KEYS; i++) {
+		if (strlen(store[i] == 0) {
+			
+		}
+	}
+}
+
+
+
+
+
